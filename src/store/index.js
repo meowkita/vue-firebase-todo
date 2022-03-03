56417a539dc422bb4891d5aeb2fbe83db208053b
Nikita Lossinov <nikita.lossinov@gmail.com>
2022-03-03 22:01:16 +0600

    Bounded store and auth

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,40 @@
 import { createStore } from "vuex";
+import { auth } from "@/firebase";
 
 export default createStore({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
+  state: {
+    user: null,
+  },
+  getters: {
+    isAuthenticated(state) {
+      return !!state.user;
+    },
+  },
+  mutations: {
+    login(state, user) {
+      state.user = user;
+    },
+  },
+  actions: {
+    async loginWithEmailAndPassword({ commit }, { email, password }) {
+      await auth
+        .signInWithEmailAndPassword(auth.getAuth(), email, password)
+        .then((response) => {
+          console.log(response);
+          commit("login", response.user);
+        })
+        .catch((error) => {
+          throw error;
+        });
+    },
+    async loginWithGoogle({ commit }) {
+      await auth
+        .signInWithPopup(auth.getAuth(), new auth.GoogleAuthProvider())
+        .then((response) => {
+          console.log(response);
+          commit("login", response.user);
+        });
+    },
+  },
   modules: {},
 });
