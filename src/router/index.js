6cdc4b7ec62ba46a8674bb4cd2c93c023ea61e03
Nikita Lossinov <nikita.lossinov@gmail.com>
2022-03-03 19:53:24 +0600

    Routes created

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
+import SignInView from "@/views/SignInView";
 
-const routes = [];
+const routes = [
+  {
+    path: "/sign-in",
+    name: "sign-in",
+    component: SignInView,
+  },
+  {
+    path: "/sign-up",
+    name: "sign-up",
+    component: () => import("@/views/SignUpView"),
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: () => import("@/views/HomeView"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
