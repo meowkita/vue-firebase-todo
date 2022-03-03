ca06dc8105cb2a1d7dbd0c51ab4ba22be6b735cb
Nikita Lossinov <nikita.lossinov@gmail.com>
2022-03-03 20:33:42 +0600

    Created ToDo adder

---
+++ src/components/TodoItemCreator.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="form-block mb-3">
+      <label class="form-label" for="header">Header</label>
+      <input class="form-control" type="text" />
+    </div>
+    <div class="form-block mb-3">
+      <label class="form-label" for="content">Content</label>
+      <textarea
+        class="form-control"
+        style="resize: none"
+        rows="3"
+        name="content"
+        id="content"
+      ></textarea>
+    </div>
+    <div class="form-block text-end">
+      <button class="btn btn-primary">Create</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItemCreator",
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="container mt-4">
     <div class="row">
       <div class="col-12 col-md-4 col-lg-3 mb-3 mb-md-0">
-        <h2>User Info</h2>
+        <h1>User Info</h1>
         <div>
           <h5>Email</h5>
           <p>name.surname@example.com</p>
@@ -10,6 +10,8 @@
         <button class="btn btn-danger btn-sm">Log Out</button>
       </div>
       <div class="col-12 col-md-8 col-lg-9 mt-2">
+        <TodoItemCreator />
+        <hr />
         <ul class="list-group">
           <ToDoListItem />
         </ul>
@@ -20,9 +22,10 @@
 
 <script>
 import ToDoListItem from "@/components/TodoListItem.vue";
+import TodoItemCreator from "@/components/TodoItemCreator.vue";
 
 export default {
   name: "HomeView",
-  components: { ToDoListItem },
+  components: { ToDoListItem, TodoItemCreator },
 };
 </script>
