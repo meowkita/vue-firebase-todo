0b2006dc03842dd038176647acc7cb3526298291
Nikita Lossinov <nikita.lossinov@gmail.com>
2022-03-03 23:27:21 +0600

    Added user profile picture on home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,14 @@
     <div class="row">
       <div class="col-12 col-md-4 col-lg-3 mb-3 mb-md-0">
         <h1>User Info</h1>
+        <img :src="this.$store.state.user.photoURL" />
+        <div v-if="this.$store.state.user.displayName">
+          <h5>Display Name</h5>
+          <p>{{ this.$store.state.user.displayName }}</p>
+        </div>
         <div>
           <h5>Email</h5>
-          <p>name.surname@example.com</p>
+          <p>{{ this.$store.state.user.email }}</p>
         </div>
         <button
           class="btn btn-danger btn-sm"
