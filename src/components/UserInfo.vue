9c74f99b4c6a50dad559c50a9566b3596adb5590
Nikita Lossinov <nikita.lossinov@gmail.com>
2022-03-05 15:54:30 +0600

    User info component

---
+++ src/components/UserInfo.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>User Info</h1>
+  <div v-if="!!this.$store.state.auth.user.photoURL">
+    <img
+      class="mb-3 rounded-pill"
+      :src="this.$store.state.auth.user.photoURL"
+    />
+  </div>
+  <div v-if="!!this.$store.state.auth.user.displayName">
+    <h5>Display Name</h5>
+    <p>{{ this.$store.state.auth.user.displayName }}</p>
+  </div>
+  <div>
+    <h5>Email</h5>
+    <p>{{ this.$store.state.auth.user.email }}</p>
+  </div>
+  <button
+    class="btn btn-danger btn-sm"
+    @click="this.$store.dispatch('signOut')"
+  >
+    Log Out
+  </button>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,28 +2,13 @@
   <div class="container mt-4">
     <div class="row">
       <div class="col-12 col-md-4 col-lg-3 mb-3 mb-md-0">
-        <h1>User Info</h1>
-        <img :src="this.$store.state.user.photoURL" />
-        <div v-if="this.$store.state.user.displayName">
-          <h5>Display Name</h5>
-          <p>{{ this.$store.state.user.displayName }}</p>
-        </div>
-        <div>
-          <h5>Email</h5>
-          <p>{{ this.$store.state.user.email }}</p>
-        </div>
-        <button
-          class="btn btn-danger btn-sm"
-          @click="this.$store.dispatch('signOut')"
-        >
-          Log Out
-        </button>
+        <user-info />
       </div>
       <div class="col-12 col-md-8 col-lg-9 mt-2">
-        <TodoItemCreator />
+        <todo-item-creator />
         <hr />
         <ul class="list-group">
-          <ToDoListItem />
+          <todo-list-item />
         </ul>
       </div>
     </div>
@@ -31,11 +16,15 @@
 </template>
 
 <script>
-import ToDoListItem from "@/components/TodoListItem.vue";
+import UserInfo from "@/components/UserInfo.vue";
+import TodoListItem from "@/components/TodoListItem.vue";
 import TodoItemCreator from "@/components/TodoItemCreator.vue";
 
 export default {
   name: "HomeView",
-  components: { ToDoListItem, TodoItemCreator },
+  components: { TodoListItem, TodoItemCreator, UserInfo },
+  mounted() {
+    this.$store.dispatch("fetchTodoList");
+  },
 };
 </script>
