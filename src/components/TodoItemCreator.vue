2a7b80f5efc51b10d0390074bf7ec5ec7ada9fe0
Nikita Lossinov <nikita.lossinov@gmail.com>
2022-03-06 22:58:30 +0600

    ItemCreator refactor

--- src/components/TodoItemCreator.vue
+++ src/components/TodoItemCreator.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="form-block mb-3">
       <label class="form-label" for="header">Header</label>
-      <input class="form-control" type="text" />
+      <input class="form-control" type="text" v-model="this.newTodo.header" />
     </div>
     <div class="form-block mb-3">
       <label class="form-label" for="content">Content</label>
@@ -12,10 +12,11 @@
         rows="3"
         name="content"
         id="content"
+        v-model="this.newTodo.content"
       ></textarea>
     </div>
     <div class="form-block text-end">
-      <button class="btn btn-primary">Create</button>
+      <button class="btn btn-primary" @click="addNewTodo">Create</button>
     </div>
   </div>
 </template>
@@ -23,5 +24,22 @@
 <script>
 export default {
   name: "TodoItemCreator",
+  data() {
+    return {
+      newTodo: {
+        header: "",
+        content: "",
+        isChecked: false,
+        author: "",
+      },
+    };
+  },
+  methods: {
+    async addNewTodo() {
+      await this.$store.dispatch("addNewTodo", this.newTodo);
+      this.newTodo.header = "";
+      this.newTodo.content = "";
+    },
+  },
 };
 </script>
